6a6059470883b7688778fafdaf5036fc57ba3397
Chaitanya Agarwal <chaipanch98@gmail.com>
2023-06-17 13:12:51 +0530

    Added Example

--- Lecture 1/REACT/app.js
+++ Lecture 1/REACT/app.js
@@ -18,17 +18,31 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // );
 
 //Example 4 - Creating an element with multiple child elements
+// const helloWorld = React.createElement("div", { id: "parent" }, [
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("p", { id: "child" }, "This is a Child Div 1")
+//   ),
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("p", { id: "child" }, "This is a Child Div 2")
+//   ),
+// ]);
+
+//Example 5 - Creating an element with multiple child elements
 const helloWorld = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("p", { id: "child" }, "This is a Child Div 1")
-  ),
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("p", { id: "child" }, "This is a Child Div 2")
-  ),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h3", { id: "child" }, "This is a Parent Div 1"),
+    React.createElement("p", { id: "child" }, "This is a Child Div 1"),
+    React.createElement("p", { id: "child" }, "This is a Child Div 2"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h3", { id: "child" }, "This is a Parent Div 2"),
+    React.createElement("p", { id: "child" }, "This is a Child Div 1"),
+    React.createElement("p", { id: "child" }, "This is a Child Div 2"),
+  ]),
 ]);
 
 root.render(helloWorld);
